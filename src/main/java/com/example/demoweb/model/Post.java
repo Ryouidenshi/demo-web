b3944100995879ee8bfe7aea1148b5a0f3876016
Ryouidenshi <djonich@mail.ru>
2021-09-12 20:12:21 +0500

    6.2

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,15 +1,23 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private int likes;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text=text;
+        this.creationDate = creationDate;
     }
 
     public int getLikes() {
         return likes;
+    }
+
+    public Date getCreationDate(){
+        return creationDate;
     }
 
     public String getText() {
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,15 +3,17 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.*;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
+        Date date = new Date();
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post("Самый первый пост"));
-        posts.add(new Post("Второй пост"));
-        posts.add(new Post("Третий пост"));
+        posts.add(new Post("Самый первый пост", date));
+        posts.add(new Post("Второй пост", date));
+        posts.add(new Post("Третий пост", date));
         return posts;
     }
 }
